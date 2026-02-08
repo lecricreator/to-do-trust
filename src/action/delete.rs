760433ed24305f3662928a507d94c67800712b73
Louca <loucamorales@orange.fr>
2026-02-09 00:59:52 +0100

    try to implement delete, but not work

--- src/action/delete.rs
+++ src/action/delete.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::{Write}};
+use std::{fmt::format, fs, io::Write};
 use crate::gestionary_file::{self};
 use crate::errors::{self};
 
@@ -8,5 +8,13 @@ pub fn delete(argc: usize, args: &Vec<String>) {
         Ok(l) => l,
         Err(_e) => return,
     };
-    fs::remove_file("a.txt").expect("Cannot remove file.");
+    let total_name: String = format!("{}.todoR", args[2]);
+    match fs::remove_file(total_name) {
+        Ok(_l) => return,
+        Err(_e) => (),
+    }
+    match fs::remove_file(&args[2]) {
+        Ok(_l) => return,
+        Err(_e) => {errors::print_error(errors::ErrorName::err_file_not_found, args[2].clone()); return}
+    }
 }
