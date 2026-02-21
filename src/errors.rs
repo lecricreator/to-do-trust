a7fd43e3f9783d8034196ea26f0a12cd9873aa2a
Louca <loucamorales@orange.fr>
2026-02-21 23:54:40 +0100

    Clean the errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -1,18 +1,6 @@
 use colored::Colorize;
 use thiserror::Error;
 
-pub const FILE_NOT_FOUND: &str = ".todoR file '{}' not exist.\nTap 'list' for see all the .todoR file.";
-pub const NO_SUFFISALY_ARGS: &str = "Need {} arguments.\n1: todoR \n2: action\n3. name of file";
-//"todorustlist add name_of_todorustlist_file write_task (optionnal)write_comentary"
-#[derive(PartialEq)]
-pub enum ErrorName {
-        ErrFileNotFound,
-        ErrReadDirectory,
-        ErrLenTooBig,
-        ErrActionNotExist,
-        ErrNotSuffisalyArg(usize),
-}
-
 #[derive(Error, Debug)]
 pub enum MyError {
     #[error("Not enough argument. Filename needed.")]
@@ -51,35 +39,4 @@ pub enum MyError {
     IoError(#[from] std::io::Error),
     #[error("varrerror env")]
     EnvError(#[from] std::env::VarError),
-}
-
-pub fn print_error (err: ErrorName, err_msg: String) {
-    if err == ErrorName::ErrFileNotFound {
-        let red_print = format!(".todoR file '{}.todoR' not exist or cannot be read or modified.\n", err_msg).red();
-        eprintln!("{}Tap 'list' for see all the .todoR file and verified if it exist.", red_print);
-    }else if err == ErrorName::ErrReadDirectory {
-        let red_print = format!("Error reading directory: {}.\n", err_msg).red();
-        eprintln!("{}", red_print);
-    }else if err == ErrorName::ErrNotSuffisalyArg(3) {
-        let red_print = format!("Need {} arguments.\n", err_msg).red();
-        eprintln!("{}1: todoR \n2: action\n3. name of todoR file", red_print);
-    }else if err == ErrorName::ErrNotSuffisalyArg(5) {
-        let red_print = format!("Need {} arguments.\n", err_msg).red();
-        eprintln!("{}1: todoR \n2: add\n3. name of.todoR file\n4. write_task\n5. (optionnal)write_comentary", red_print);
-    }
-}
-
-pub fn verified_arg(argc: usize, nbr_arg: usize) -> bool {
-    let conv_to_str:String = nbr_arg.to_string();
-    if argc != nbr_arg {
-        if nbr_arg == 2 {
-            print_error(ErrorName::ErrNotSuffisalyArg(2), conv_to_str);
-        } else if nbr_arg == 3 {
-            print_error(ErrorName::ErrNotSuffisalyArg(3), conv_to_str);
-        } else if nbr_arg == 5 {
-            print_error(ErrorName::ErrNotSuffisalyArg(5), conv_to_str);
-        }
-        return false
-    }
-    return true
 }
